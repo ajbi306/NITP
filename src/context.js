5799937f27b1975ddf756fdb72a4ac0f1f7f65cf
ajbi306 <107359720+ajbi306@users.noreply.github.com>
2023-07-03 00:00:22 +0530

    Add files via upload

---
+++ src/context.js
@@ -0,0 +1,34 @@
+import {createContext, useEffect, useReducer}  from "react"
+
+import Reducer from "./reducer";
+
+const Intial_state = {
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    isfetching: false,
+    error: false,
+};
+
+export const Context = createContext(Intial_state);
+
+export const ContextProvider = ({children}) => {
+  
+  const [state, dispatch] = useReducer(Reducer, Intial_state);
+  
+  useEffect(() =>{
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+  
+  return(
+    <Context.Provider value = {
+            {
+                user: state.user,
+                isfetching: state.isfetching,
+                error: state.error,
+                dispatch,
+            }
+        }
+        > 
+        {children}
+      </Context.Provider>
+  );
+};
